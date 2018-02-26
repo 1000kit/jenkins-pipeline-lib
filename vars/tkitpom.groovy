2d8c4d6dd19b7589ba4c40ebe8fa814cdbd1b41b
tasgit <tasgit@1000kit.org>
2018-02-26 18:48:42 +0100

    smaler fix

--- vars/tkitpom.groovy
+++ vars/tkitpom.groovy
@@ -4,15 +4,15 @@ def version(String path) {
     def contents = readFile(path)
     def project = new XmlSlurper().parseText(contents)
     def version = project.version.text().trim()
-    echo "parsed Version $version"
+    echo "parsed POM Version $version"
     return version
 }
 
 def majorVersion(String path) {
     def version = version(path)
     if(version.count(".") >= 1){
-        def major = version.trim().split("\\.")[0]
-        echo "parsed Version $major"        
+        def major = version.trim().tokenize("\\.-")[0]
+        echo "parsed major $major"        
         return major
     }
     echo "version count <2"
@@ -22,8 +22,8 @@ def majorVersion(String path) {
 def minorVersion(String path) {
     def version = version(path)
     if(version.count(".") >= 1){
-        def minor = version.trim().split("\\.")[1]
-        echo "parsed Version $minor"        
+        def minor = version.trim().tokenize("\\.-")[1]
+        echo "parsed minor $minor"        
         return minor
     }
     echo "version count <2"
@@ -33,8 +33,8 @@ def minorVersion(String path) {
 def patchVersion(String path) {
     def version = version(path)
     if(version.count(".") >= 2){
-        def patch = version.trim().split("\\.")[2]
-        echo "parsed Version $patch"        
+        def patch = version.trim().tokenize("\\.-")[2]
+        echo "parsed patch $patch"        
         return patch
     }
     echo "version count <2"    
@@ -44,8 +44,8 @@ def patchVersion(String path) {
 def buildVersion(String path) {
     def version = version(path)
     if(version.count(".") >= 3){
-        def build = version.trim().split("\\.")[3]
-        echo "parsed Version $build"        
+        def build = version.trim().tokenize("\\.-")[3]
+        echo "parsed build $build"        
         return build
     }
     echo "version count <3"    
