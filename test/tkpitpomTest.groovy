9a78eba78af5761005c41ad781572a2dd48ea73d
tasgit <tasgit@1000kit.org>
2018-02-26 22:28:49 +0100

    add test and update def

---
+++ test/tkpitpomTest.groovy
@@ -0,0 +1,44 @@
+import org.junit.BeforeClass
+import org.junit.Test
+import org.tkit.jenkins.pipeline.lib.PomVersion
+import tkitpom
+
+class tkpitpomTest {
+
+
+    @BeforeClass
+    static void setup() {
+        tkitpom.metaClass.echo {
+            println it
+            return it
+        }
+    }
+
+    @Test
+    void getMajorVersion() {
+    	def tkp = new tkitpom()
+    	def pf = 'pomTest.xml'
+    	def pomVersion = tkp.readVersion(pf)
+  
+		def major = pomVersion.getMajor() 
+		println "major: $major"
+        assert major != null
+        assert major == "1"
+        
+       	def minor = pomVersion.getMinor() 
+       	println "minor: $minor"
+        assert minor != null
+        assert minor == "2"
+        
+        def patch = pomVersion.getPatch()
+        println "patch: $patch" 
+        assert patch != null
+        assert patch == "3" 
+        
+        def build = pomVersion.getBuild() 
+        println "build: $build"
+        assert build != null
+        assert build == "SNAPSHOT"     
+    }
+
+}
--- vars/tkitpom.groovy
+++ vars/tkitpom.groovy
@@ -1,27 +1,34 @@
-#!/usr/bin/env groovy
+#!groovy
+import org.tkit.jenkins.pipeline.lib.PomVersion
 
-def version(String path) {
-    def contents = readFile(path)
-    def project = new XmlSlurper().parseText(contents)
-    def version = project.version.text().trim()
-    PomVersion pomVersion = new PomVersion()
-    pomVersion.setVersion(version)
+
+def readVersion(path) {
+    echo "reading pom file $path"
+    def pomVersion = new PomVersion()
+    try {
+    	def contents = readFile(path)
+    	def project = new XmlSlurper().parseText(contents)
+    	def version = project.version.text().trim()
+    	pomVersion.setVersion(version)
+    } catch (Exception ex) {
+    	echo "caught exception $ex"
+    }
     return pomVersion
 }
 
-def majorVersion(PomVersion pomVersion) {
+def majorVersion(pomVersion) {
     return pomVersion.getMajor()
 }
 
-def minorVersion(PomVersion pomVersion) {
+def minorVersion(pomVersion) {
     return pomVersion.getMinor()
 }
 
-def patchVersion(PomVersion pomVersion) {
+def patchVersion(pomVersion) {
     return pomVersion.getPatch()
 }
 
-def buildVersion(PomVersion pomVersion) {
+def buildVersion(pomVersion) {
     return pomVersion.getBuild()
 }
 
